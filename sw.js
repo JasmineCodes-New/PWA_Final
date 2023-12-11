4adeb91f409ee79ddec34bef3f58a91b897ea053
Jasmine Irvin <jasmine.irvin@ymail.com>
2023-12-11 12:44:44 -0500

    updating service worker code

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 //console.log('service worker inside sw.js');
 
-const cacheName = "app-shell-rsrs";
+const cacheName = "app-shell-rsrs-v1";
 const assets = [
     'index.html',
     'js/app.js',
@@ -9,17 +9,19 @@ const assets = [
     'css/styles.css',
     'css/materialize.min.css',
     'img/pwacontacts.png',
-    'https://fonts.googleapis.com/icon?family=Material+Icons'
-]
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://fonts.gstatic.com/s/materialicons/v140/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
+];
 
-
-caches.open(cacheName).then(cache => {
-    cache.addAll(assets);
-})
 
 //install service worker
 self.addEventListener('install', evt => {
-    console.log('service worker has been installed.');
+    //console.log('service worker has been installed.');
+    evt.waitUntil(
+        caches.open(cacheName).then(cache => {
+            cache.addAll(assets);
+        })
+    );
 });
 
 //activate
@@ -29,5 +31,9 @@ self.addEventListener('activate', evt => {
 
 //fetch event
 self.addEventListener('fetch', evt => {
-    console.log(evt);
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request);
+        })
+    );
 });
