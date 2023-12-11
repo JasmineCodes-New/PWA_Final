4268d46db7d36a89216e023e70836327e9e8a78f
Jasmine Irvin <jasmine.irvin@ymail.com>
2023-12-11 14:01:50 -0500

    mostly finished with service worker

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 //check if the service-worker is supported
-if('serviceWorker' in navigator){
-
-    navigator.serviceWorker.register('/sw.js')
-    .then((reg) => console.log('service worker registered', reg))
-    .catch((err) => console.log('service worker not registered', err))
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register('/sw.js')
+            .then(registration => {
+                console.log('Service worker registered:', registration);
+            })
+            .catch(error => {
+                console.log('Service worker registration failed:', error);
+            });
+    });
 }
--- sw.js
+++ sw.js
@@ -9,31 +9,83 @@ const assets = [
     'css/styles.css',
     'css/materialize.min.css',
     'img/pwacontacts.png',
+    'pages/about.html',
+    'pages/default.html',
     'https://fonts.googleapis.com/icon?family=Material+Icons',
     'https://fonts.gstatic.com/s/materialicons/v140/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
 ];
 
+//cache size limit function
+const limitCacheSize = (name, size) => {
+    caches.open(name).then(async cache => {
+        const keys = await cache.keys();
+        if (keys.length > size) {
+            cache.delete(keys[0]).then(() => limitCacheSize(name, size));
+        }
+    });
+};
 
-//install service worker
-self.addEventListener('install', evt => {
-    //console.log('service worker has been installed.');
-    evt.waitUntil(
+
+self.addEventListener('install', event => {
+    console.log('Service Worker: Installed');
+    event.waitUntil(
         caches.open(cacheName).then(cache => {
-            cache.addAll(assets);
+            console.log('Service Worker: Caching Files');
+            return cache.addAll(assets);
+        }).catch(error => {
+            console.error('Service Worker: Cache failed', error);
         })
     );
 });
 
-//activate
-self.addEventListener('activate', evt => {
-    console.log('service worker has been activated.');
-});
-
-//fetch event
-self.addEventListener('fetch', evt => {
-    evt.respondWith(
-        caches.match(evt.request).then(cacheRes => {
-            return cacheRes || fetch(evt.request);
+self.addEventListener('activate', event => {
+    console.log('Service Worker: Activated');
+    event.waitUntil(
+        caches.keys().then(cacheKeys => {
+            return Promise.all(
+                cacheKeys.map(key => {
+                    if (key !== cacheName) {
+                        console.log('Service Worker: Clearing Old Cache');
+                        return caches.delete(key);
+                    }
+                })
+            );
         })
     );
 });
+
+self.addEventListener('fetch', event => {
+    console.log('Service Worker: Fetching', event.request.url);
+
+    if (event.request.destination === 'document') {
+        event.respondWith(
+            caches.match(event.request).then(cacheRes => {
+                if (cacheRes) {
+                    return cacheRes; // Respond with cached content if available
+                }
+
+                // If not cached, fetch from the network and cache dynamically
+                return fetch(event.request).then(fetchRes => {
+                    return caches.open(cacheName).then(cache => {
+                        cache.put(event.request, fetchRes.clone());
+                        return fetchRes;
+                    });
+                }).catch(error => {
+                    console.error('Service Worker: Fetch failed', error);
+                    // return a fallback response for failed fetches
+                    // return new Response('Fallback content here', { headers: { 'Content-Type': 'text/html' } });
+                });
+            })
+        );
+    }
+    // For other file types like CSS, JS, images, handle as before
+    else {
+        // Existing logic for caching other file types
+        event.respondWith(
+            caches.match(event.request).then(cacheRes => {
+                return cacheRes || fetch(event.request);
+            })
+        );
+    }
+});
+
