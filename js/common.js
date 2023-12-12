dd4c858972def02a4be25b829e417bed5b4438f3
Jasmine Irvin <jasmine.irvin@ymail.com>
2023-12-11 22:36:45 -0500

    Delete js directory

--- js/common.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  // Nav menu
-  const menus = document.querySelectorAll('.side-menu');
-  M.Sidenav.init(menus, { edge: 'left' });
-  // Add contacts form
-  const forms = document.querySelectorAll('.side-form');
-  M.Sidenav.init(forms, { edge: 'right' });
-});
